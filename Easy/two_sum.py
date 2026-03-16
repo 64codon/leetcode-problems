16c651efe52a390507cd0890d081e19a6821182f
64codon <kosoossoss@gmail.com>
2026-03-17 00:51:29 +0200

    Code Optimization: Some silly stuff I overlooked while solving the Two Sums problem

--- Easy/two_sum.py
+++ Easy/two_sum.py
@@ -3,16 +3,15 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         num_dict = dict()
-        arr = []
         index = 0
         for curr in nums:
             if (target - curr) in num_dict:
+                arr = []
                 arr.append(num_dict[target - curr])
                 arr.append(index)
-                break
+                return arr
             num_dict[curr] = index
             index += 1
-        return arr
     
 test = Solution()
 nums = [20, 30, 12, 38, 21, 40]
