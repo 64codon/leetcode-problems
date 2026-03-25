23f3d14b438aebe9ffcfe8cdeee346be1c31ea0f
64codon <kosoossoss@gmail.com>
2026-03-25 05:12:50 +0200

    Solution: Reverse Linked List

---
+++ 01. Easy/reverse_linked_list.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def myList(self):
+        self.head = None
+
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        new_list = self.myList()
+        
+
